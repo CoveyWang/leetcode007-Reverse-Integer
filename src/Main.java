aa93e513ed44d2d6bc9be57db2a70987936387d3
CoveyWang <ww8492032@sohu.com>
2015-02-13 16:08:38 +0800

    AC

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        //long a=9646324351l;
+        System.out.println(solution.reverse(-2147483648));
+    }
+}
---
+++ src/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            if (Math.abs(rev) > 214748364) {
+                return 0;
+            }
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        return rev;
+    }
+}
